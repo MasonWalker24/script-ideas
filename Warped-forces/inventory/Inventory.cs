ff18077d36832a2a557082d6a88142608017363c
Mason Walker <46076583+cubesniper24@users.noreply.github.com>
2021-10-12 10:23:42 -0400

    Update Inventory.cs

--- Warped-forces/inventory/Inventory.cs
+++ Warped-forces/inventory/Inventory.cs
@@ -9,8 +9,32 @@ public class InvSlot
     Item storedItem = storedItem;
   }
 }
-void addItemToInv()
+public class Item
 {
+  public Item(string name, GameObject obj)
+  {
+    String ItemName = name;
+    GameObject ShownObj = obj;
+  }
+}
+public GameObject testItem;
+private void start()
+{
+  Item test = new Item("test item", testItem);
+}
+void addItemToInv(Item itemToStore)
+{
+  bool finished = false;
   for(slot of Slots)
   {
-    
+    if(slot.storedItem == null && finished == false)
+    {
+      slot.storedItem = itemToStore
+      finished = true;
+    }
+  }
+}
+public void awake()
+{
+  addItemToInv(test);
+}
