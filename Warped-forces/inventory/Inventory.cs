0bc8f15efb02bc1df6dfccae57edb36a6f4e9915
Mason Walker <46076583+cubesniper24@users.noreply.github.com>
2021-10-12 14:13:53 -0400

    Update Inventory.cs

--- Warped-forces/inventory/Inventory.cs
+++ Warped-forces/inventory/Inventory.cs
@@ -7,15 +7,17 @@ public class InvSlot
   public InvSlot(Item storedItem)
   {
     Item storedItem = storedItem;
+    int currentStackSize;
   }
 }
 public class Item
 {
-  public Item(string name, GameObject obj, int size)
+  public Item(string name, GameObject obj, int size, string itemId)
   {
     String ItemName = name;
     GameObject ShownObj = obj;
     int maxStackSize = size;
+    string itemId = itemId;
   }
 }
 public GameObject testItem;
@@ -28,10 +30,13 @@ void addItemToInv(Item itemToStore)
   bool finished = false;
   for(slot of Slots)
   {
-    if((slot.storedItem == null && finished == false) || ())
+    if(slot.storedItem == null && finished == false)
     {
       slot.storedItem = itemToStore
       finished = true;
+    } else if(slot.storedItem.itemId == itemToStore.itemId && slot.currentStackSize < itemToStore.maxStackSize)
+    {
+      slot.currentStackSize += 1;
     }
   }
 }
