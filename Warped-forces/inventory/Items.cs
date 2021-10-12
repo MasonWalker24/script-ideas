cccd033e7ee5c1c31394d741af1eb31ac7609fe9
Mason Walker <46076583+cubesniper24@users.noreply.github.com>
2021-10-12 09:17:12 -0400

    Delete Items.cs

--- Warped-forces/inventory/Items.cs
+++
@@ -1,9 +0,0 @@
-public class Item
-{
-  public Item(string name, GameObject obj)
-  {
-    String ItemName = name;
-    GameObject ShownObj = obj;
-  }
-}
-Item WoodenSword = new Item("Wooden Sword", WdnSwrd);
