114f2219fe33074d1003ec7f3469b0f56d636718
Mason Walker <46076583+cubesniper24@users.noreply.github.com>
2021-10-14 10:19:37 -0400

    Update Inventory.cs

--- Warped-forces/inventory/Inventory.cs
+++ Warped-forces/inventory/Inventory.cs
@@ -8,6 +8,26 @@ public class Inventory : MonoBehaviour
     Item test = new Item("test item", testItem, 2);
     InvSlots = new GameObject[23];
   }
+  void addItemToInv(Item itemToStore)
+{
+  bool finished = false;
+  for(slot of Slots)
+  {
+    if(slot.storedItem == null && finished == false)
+    {
+      slot.storedItem = itemToStore
+      finished = true;
+      image = Instantiate(itemToStore.objImage, Quaternion.identity);
+      image.transform.parent = slot;
+      image.transform.position.x = 0;
+      image.transform.position.y = 0;
+      slot.currentStackSize = 1;
+    } else if(slot.storedItem.itemId == itemToStore.itemId && slot.currentStackSize < itemToStore.maxStackSize && finished == false)
+    {
+      slot.currentStackSize += 1;
+    }
+  }
+}
 }
 public class InvSlot
 {
@@ -33,26 +53,7 @@ public class Item
     itemId = itemid;
   }
 }
-void addItemToInv(Item itemToStore)
-{
-  bool finished = false;
-  for(slot of Slots)
-  {
-    if(slot.storedItem == null && finished == false)
-    {
-      slot.storedItem = itemToStore
-      finished = true;
-      image = Instantiate(itemToStore.objImage, Quaternion.identity);
-      image.transform.parent = slot;
-      image.transform.position.x = 0;
-      image.transform.position.y = 0;
-      slot.currentStackSize = 1;
-    } else if(slot.storedItem.itemId == itemToStore.itemId && slot.currentStackSize < itemToStore.maxStackSize && finished == false)
-    {
-      slot.currentStackSize += 1;
-    }
-  }
-}
+
 public void moveItem()
 {
   GameObject
